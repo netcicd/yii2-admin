ee246c474648be5f8565a8ac543e00e3b248fa1a
Mark Sibering <46484069+netcicd@users.noreply.github.com>
2024-04-29 09:02:54 +0200

    Update AuthItem.php
    fix php 8.1 error

--- models/searchs/AuthItem.php
+++ models/searchs/AuthItem.php
@@ -76,12 +76,12 @@ class AuthItem extends Model
         $this->load($params);
         if ($this->validate()) {
 
-            $search = mb_strtolower(trim((string)$this->name));
-            $desc = mb_strtolower(trim((string)$this->description));
+            $search = mb_strtolower(trim((string)$this->name ?? ''));
+            $desc = mb_strtolower(trim((string)$this->description ?? ''));
             $ruleName = $this->ruleName;
             foreach ($items as $name => $item) {
-                $f = (empty($search) || mb_strpos(mb_strtolower($item->name), $search) !== false) &&
-                    (empty($desc) || mb_strpos(mb_strtolower($item->description), $desc) !== false) &&
+                $f = (empty($search) || mb_strpos(mb_strtolower($item->name ?? ''), $search) !== false) &&
+                    (empty($desc) || mb_strpos(mb_strtolower($item->description ?? ''), $desc) !== false) &&
                     (empty($ruleName) || $item->ruleName == $ruleName);
                 if (!$f) {
                     unset($items[$name]);
